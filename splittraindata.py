61aa053c81a7153921174fce34e48f6353fc4f88
Mikolaj Pazdzior <mikolajpaz@gmail.com>
2025-01-06 21:51:41 +0100

    Splitting training data to training and validation

---
+++ splittraindata.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def split_train_data(path, train_path, val_path):
+    # Wczytaj dane
+    train_data = pd.read_csv(path)
+
+    # Podziel dane na treningowe i walidacyjne
+    train_data, val_data = train_test_split(train_data, test_size=0.2, random_state=42)
+
+    # Zapisz podzielone dane
+    train_data.to_csv(train_path, index=False)
+    val_data.to_csv(val_path, index=False)
+
+if __name__ == "__main__":
+    split_train_data("dane/training.csv", "dane/train_split.csv", "dane/val_split.csv")
